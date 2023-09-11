4d3ff9569b0132a17b281d94054d8908cbbf3390
agnesem670 <agnesem670@gmail.com>
2023-09-11 11:17:47 +0200

    outputting lists

--- app.js
+++ app.js
@@ -4,26 +4,16 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'Title from data return',
-            author: 'Name',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                {title: "name of the wind", author: "patric r."},
+                {title: "the way of kings", author: "brandon s."},
+                {title: "the final empire", author: "brandon s."},
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
-        },
-        handleEvent(e, data) {
-            console.log (e, e.type)
-            if (data) {
-                console.log(data)
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
         }
     }
 })
