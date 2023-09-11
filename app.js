60d8b1399a7bd8d701b4e7b16d7510ce18648256
agnesem670 <agnesem670@gmail.com>
2023-09-11 13:24:24 +0200

    dynamic classes

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ const app = Vue.createApp({
             url: 'http://hoborg.pl/react',
             showBooks: true,
             books: [
-                {title: "name of the wind", author: "patric r.", img:""},
-                {title: "the way of kings", author: "brandon s.", img:""},
-                {title: "the final empire", author: "brandon s.", img:""},
+                {title: "name of the wind", author: "patric r.", img:"", isFav: true },
+                {title: "the way of kings", author: "brandon s.", img:"", isFav: false},
+                {title: "the final empire", author: "brandon s.", img:"", isFav: true},
             ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
         }
     }
 })
